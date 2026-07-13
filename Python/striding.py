3a7322260721210acfdfcf0c52a3ba6c2b0e0045
kavyasri kota <Kavyasrikota989@gmail.com>
2026-07-13 17:04:46 +0530

    Add files via upload

---
+++ Python/striding.py
@@ -0,0 +1,50 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #striding
+>>> a="data science"
+>>> a[::1]
+'data science'
+>>> a[::2]
+'dt cec'
+>>> a[::]
+'data science'
+>>> a="cloud computing"
+>>> a[::4]
+'cdmi'
+>>> a[::3]
+'cucpi'
+>>> a[::6]
+'cci'
+>>> a[3:6]
+'ud '
+>>> a[:9]
+'cloud com'
+>>> a[7:]
+'omputing'
+>>> a="machine learning"
+>>> a[1:10:3]
+'ai '
+>>> a[2:14:4]
+'cea'
+>>> a[3:15:5]
+'hli'
+>>> a[1:12:2]
+'ahn er'
+>>> #negative striding"
+>>> a="python course"
+>>> a[-1:-6:-2]
+'ero'
+>>> a[-1:-11:-4]
+'eoo'
+>>> a[-3:-13:-5]
+'rn'
+>>> a[-5:-12:--6]
+''
+>>> a[-5:-12:-6]
+'ot'
+>>> a[-2:-9:-1]
+'sruoc n'
+>>> a[-6:-13:-3]
+'coy'
+a[-4:-12:-6]
+'uh'
