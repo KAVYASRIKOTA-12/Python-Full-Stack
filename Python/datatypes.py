8993654eb56b205ae72de71ba23f8b3585b1a609
kavyasri kota <Kavyasrikota989@gmail.com>
2026-07-13 16:50:21 +0530

    Add files via upload

---
+++ Python/datatypes.py
@@ -0,0 +1,35 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #datatypes
+>>> a=5
+>>> type(a)
+<class 'int'>
+>>> b=6.7
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> d="python"
+>>> type(d)
+<class 'str'>
+>>> e='''course'''
+>>> type(e)
+<class 'str'>
+>>> x=4+9j
+>>> type(x)
+<class 'complex'>
+>>> y=3j+5
+>>> type(y)
+<class 'complex'>
+>>> z=6j
+>>> type(z)
+<class 'complex'>
+>>> z=True
+>>> type(z)
+<class 'bool'>
+>>> y=False
+>>> type(y)
+<class 'bool'>
+>>> y=7+9i
+SyntaxError: invalid decimal literal
