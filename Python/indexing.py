831f6b043212dbc804a1ebcd1f3a0f920c8549e4
kavyasri kota <Kavyasrikota989@gmail.com>
2026-07-13 17:03:43 +0530

    Add files via upload

---
+++ Python/indexing.py
@@ -0,0 +1,51 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#indexing
+a="i am in class"
+a[8]+a[9]+a[10]+a[11]+a[12]
+'class'
+a[5]+a[6]
+'in'
+a[2]+a[3]
+'am'
+a[1]
+' '
+>>> a[1]+a[4]+a[7]
+'   '
+>>> a="vijayawada is a royal city"
+>>> a[17]+a[18]+a[19]+a[20]+a[21]
+'oyal '
+>>> a[16]+a[17]
+'ro'
+=
+>>> a[16]+a[17]+a[18]+a[19]+a[20]
+'royal'
+>>> a[22]+a[23]+a[24]+a[25]
+'city'
+>>> a[12]+a[13]
+'s '
+>>> a[11]+a[12]
+'is'
+>>> b="vizag is a city of destiny"
+>>> b[12]+b[13]+b[14]+b[14]
+'ityy'
+>>> b[11]+b[12]+b[13]+b[14]
+'city'
+>>> b[19]+b[20]+b[21]+b[22]+b[23]+b[24]+b[25]
+'destiny'
+>>> b[0]+b[1]+b[2]+b[3]+b[4]
+'vizag'
+>>> a="I am in python class"
+>>> a[-7]+a[-8]+a[-9]+a[-10]+a[-11]+a[-12]
+'nohtyp'
+>>> a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'class'
+>>> a[-18]+a[-17]
+'am'
+>>> b="simple is better than complex"
+>>> b[-7]+b[-6]+b[-5]+b[-4]+b[-3]+b[-2]+b[-1]
+'complex'
+>>> b[-19]+b[-18]+b[-17]+b[-16]+b[-15]+b[-14]
+'better'
+>>> b[-29]+b[-28]+b[-27]+b[-26]+b[-25]+b[-24]
+'simple'
