534ef47529ff940a0a9b4f2221cb209f287cc7e4
kavyasri kota <Kavyasrikota989@gmail.com>
2026-09-03 17:05:37 +0530

    Create eval.py

---
+++ Python/eval.py
@@ -0,0 +1,20 @@
+#eval()
+'''while True:
+    a=int(input("a value"))
+    b=int(input("b value"))
+    print(a+b)'''
+
+'''while True:
+    a=float(input("a value"))
+    b=float(input("b value"))
+    print(a+b)'''
+
+'''while True:
+    a=input("a value")
+    b=input("b value")
+    print(a+b)'''
+
+'''while True:
+    a=eval(input("a value"))
+    b=eval(input("b value"))
+    print(a+b)'''
