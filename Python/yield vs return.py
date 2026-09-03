f8393a365e8f672eb208f9232c5f66fb7bfb33cb
kavyasri kota <Kavyasrikota989@gmail.com>
2026-09-03 17:04:37 +0530

    Add examples of yield and return in Python
    Demonstrate the difference between using 'return' and 'yield' in Python functions.

---
+++ Python/yield vs return.py
@@ -0,0 +1,16 @@
+#yield vs return
+'''def mygen():
+    return "python","java","c"
+print(*mygen())'''
+
+
+'''def mygen():
+    yield "vja"
+    yield "hyd"
+    yield "vzg"
+print(*mygen())
+#next()
+d=mygen()
+print(next(d))
+print(next(d))
+print(next(d))'''
