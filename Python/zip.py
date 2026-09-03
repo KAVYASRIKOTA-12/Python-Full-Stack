9c42aa76fee8e4f9a3e8ad872bf59b769a6e4401
kavyasri kota <Kavyasrikota989@gmail.com>
2026-09-03 17:06:26 +0530

    Create zip.py

---
+++ Python/zip.py
@@ -0,0 +1,14 @@
+#zip()->we can combine multiple collections into one collection
+'''a=[10,20,30,40,50]
+names=["surendar","harsha","rupesh","krishna","mohan"]
+print(a+names)
+b=zip(a,names)
+print(b)
+c=list(zip(a,names))
+print(c)
+c=tuple(zip(a,names))
+print(c)
+c=set(zip(a,names))
+print(c)
+c=dict(zip(a,names))
+print(c)'''
