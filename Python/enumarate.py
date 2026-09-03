3395ab5da3ed6422d564f089bf322c98b1f9bb5a
kavyasri kota <Kavyasrikota989@gmail.com>
2026-09-03 17:07:20 +0530

    Create enumarate.py

---
+++ Python/enumarate.py
@@ -0,0 +1,15 @@
+#enumarate()->we can give counter to the collection
+'''names=["madhu","nirupam","deekshith","guruprasad","surendar"]
+for i in range(len(names)):
+    print(i)'''
+#enumerate method
+'''b=list(enumerate(names))
+print(b)
+b=list(enumerate(names,100))
+print(b)
+b=tuple(enumerate(names))
+print(b)
+b=set(enumerate(names))
+print(b)
+b=dict(enumerate(names))
+print(b)'''
