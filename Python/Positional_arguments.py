5d83bf9a87e6d45140b3e645d26a27fd298cece0
kavyasri kota <Kavyasrikota989@gmail.com>
2026-09-03 16:56:44 +0530

    Create Positional_arguments.py

---
+++ Python/Positional_arguments.py
@@ -0,0 +1,59 @@
+#keyword and positional arguments
+'''def Details(id,name,mailid):
+    id=10
+    name="kavya"
+    mailid="kavya@gamil.com"
+    print(id,name,mailid)
+Details(id="id",name="kavya",mailid="kavya@gamil.com")'''
+
+'''def Details(id,name,mailid):
+    print(id,name,mailid)
+Details(id="id",name="name",mailid="mailid")
+Details(id=20,name="kavya",mailid="kavya@gmail.com")
+Details(id=30,name="harika",mailid="h@gmail.com")'''
+
+'''def Details(id,name,mailid):
+    print(id,name,mailid)
+Details(40,"nidhi","n@gmail.com")'''
+
+'''def Details(id,name,mailid):
+    print(id,name,mailid)
+Details("nidhi","n@gmail.com",40)'''
+
+'''def Details(id,name,mailid):
+    print(id,name,mailid)
+Details(mailid="k@gmail.com",name="kavya",id=20)'''
+
+#swapping of two variables
+'''a=10
+b=20
+a,b=b,a
+print(a)
+print(b)'''
+
+'''def values(a,b):
+    print(b,a)
+values(10,20)'''
+
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print(a)
+print(b)'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d" %(a,b))'''
